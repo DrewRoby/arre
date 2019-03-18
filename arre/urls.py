7e5ff69b6e5495399087808d15b0287d39949e09
unknown <drew.roby@gmail.com>
2019-03-18 06:57:13 -0500

    up to 7.46

--- arre/urls.py
+++ arre/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # ^ adds static files.  MEDIA_URL from settings
--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
+from .models import Listing
 
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+    
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list
+    }
+
+    return render(request, 'listings/search.html', context)
